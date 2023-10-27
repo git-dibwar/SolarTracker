fcef6c4c102c521b13ec7752477c2d46bcbfe408
Dibwarjeet Bordolai <71434149+git-dibwar@users.noreply.github.com>
2023-10-27 14:54:23 +0530

    Update FinalFinalCode.ino

--- Final/FinalFinalCode.ino
+++ Final/FinalFinalCode.ino
@@ -3,56 +3,43 @@
 #include "CytronMotorDriver.h"
 #include <SolarCalculator.h>
 #include <TimeLib.h>
-#include <DS1307RTC.h> // Include the DS1307RTC library for RTC support
-//==================================================================================
-
+#include <DS1307RTC.h>
 
 MPU6050 mpu6050(Wire);
-CytronMD motor1(PWM_DIR, 7, 8); // PWM 2 = Pin 7, DIR 2 = Pin 8.
-CytronMD motor2(PWM_DIR, 6, 3); // PWM 1 = Pin 3, DIR 1 = Pin 6.
+CytronMD motor1(PWM_DIR, 7, 8);
+CytronMD motor2(PWM_DIR, 6, 3);
 
-// Define the required yaw and pitch angles
-//int requiredYawAngle = 20;
-//int requiredPitchAngle = 25;
 bool yawCalibrated = false;
 bool pitchCalibrated = false;
-int tolerance = 2; // Set your desired tolerance level
+int tolerance = 1;
 
-//==============================================================================================================
-double latitude = 26.738180853550922;  // Jorhat latitude
-double longitude = 94.21081269176345;  // Jorhat longitude
-double utc_offset = 5.5;  // UTC offset for Jorhat (IST)
-// Refresh interval, in seconds
-int interval = 1;
-//================================================================================================
+double latitude = 26.738180853550922;
+double longitude = 94.21081269176345;
+double utc_offset = 5.5;
+int interval = 120;  // 2 minutes interval in seconds
+time_t lastAlignmentTime = 0;
+int currentAzimuth = 0; // Track the current azimuth
+int currentPitch = 0; // Track the current pitch
 
 void setup() {
   Serial.begin(19200);
-  
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
   setSyncProvider(RTC.get); // Set the RTC as the time provider
-
+  lastAlignmentTime = now(); // Initialize the last alignment time
 }
 
 void loop() {
   mpu6050.update();
-  //===================
-  static unsigned long next_millis = 0;
-  // At every interval
-  if (millis() > next_millis)
-  {
-    time_t utc = now() - utc_offset * 3600L;;
-    double az, el;
+  time_t currentTime = now();
 
-    // Calculate the solar position, in degrees
-    calcHorizontalCoordinates(utc, latitude, longitude, az, el);
+  // Calculate the solar position, in degrees
+  double az, el;
+  calcHorizontalCoordinates(currentTime - utc_offset * 3600L, latitude, longitude, az, el);
 
-    //=============================
-    //main conditional code to input here that will accept the az and el and move the motor
-    if (!yawCalibrated) {
-    // Yaw control
+
+  if (!yawCalibrated) {
     int yaw = round(int(mpu6050.getAngleZ()));
     int yawError = round(az) - yaw;
 
@@ -70,7 +57,6 @@ void loop() {
     Serial.print("Yaw Angle: ");
     Serial.println(yaw);
   } else if (!pitchCalibrated) {
-    // Pitch control
     int pitch = round(int(mpu6050.getAngleY()));
     int pitchError = round(el) - int(pitch);
 
@@ -85,17 +71,23 @@ void loop() {
         motor2.setSpeed(90); // Adjust the speed as needed
       }
     }
-
     Serial.print("Pitch Angle: ");
     Serial.println(pitch);
   }
+
+  // Check if 2 minutes have passed since the last alignment
+  if (currentTime - lastAlignmentTime >= interval) {
+    int pitchChange = round(el)-currentPitch;
+    int azimuthChange = round(az)-currentAzimuth;
+    if(azimuthChange != 0){
+     yawCalibrated = false;
+    }
+    if (pitchChange != 0) {
+       pitchCalibrated = false;
+    }  
+    currentPitch = round(el);
+    currentAzimuth = round(az);
+    lastAlignmentTime = currentTime;
     
-   //=============================
-
-    next_millis = millis() + interval * 1000L;
   }
-  //====================
-  
 }
-
-//=================================================================
