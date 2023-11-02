101750c050c3bf54e92cda548e2661d80109565f
Dibwarjeet Bordolai <71434149+git-dibwar@users.noreply.github.com>
2023-11-02 14:43:45 +0530

    Add files via upload

---
+++ resetCode/angleTime_reset.ino
@@ -0,0 +1,51 @@
+#include <MPU6050_tockn.h>
+#include "RTClib.h"
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+void setup() {
+  Serial.begin(19200);
+  // put your setup code here, to run once:
+  mpu6050.calcGyroOffsets(true);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  mpu6050.update();
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+  Serial.println("YawAngle: " + String(round(-mpu6050.getAngleZ())) + "     PitchAngle: " + String(round(mpu6050.getAngleY())));
+  delay(2000);
+}
